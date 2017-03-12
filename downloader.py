a1c9f70bb7ebe91629856474784933bc9f565d98
ZYunH <workvl@163.com>
2017-03-12 22:30:02 +0800

    ver 0.0.1
    For downloader
    1.  Add ftp download function.
    2. Add terminal mode progress bar option.
    3. Improve some function.
    4. Fix some bugs.

--- downloader.py
+++ downloader.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import socket
+import ftplib
 
 import urllib.parse
 import urllib.request
@@ -11,13 +13,13 @@ from ssl import _create_unverified_context
 class Downloader:
     """This class is used to handle the Download requests that arrive at the server."""
 
-    def __init__(self, url, filename='', block_size=1024 * 1024, headers={}, timeout=2, ssl_context=None,
-                 terminal_mode=False):
+    def __init__(self, url, filename='', work_path='', block_size=1024 * 1024, headers={}, timeout=2, ssl_context=None,
+                 terminal_mode=False, ftp_user='', ftp_password=''):
         # If filename is not specified,it is equal to the default value provided by url.
         if filename == '':
             filename = url.split('/')[-1]
-        # Parse chinese.
-        url = urllib.parse.quote(url, safe='/:?=@&')
+        # Parse chinese to ascii.
+        url = urllib.parse.quote(url, safe='/:?=@&[]')
         # Create ssl context.
         if ssl_context:
             self.ssl_context = ssl_context
@@ -25,10 +27,14 @@ class Downloader:
             self.ssl_context = _create_unverified_context()
         # Determine whether terminal or not.
         self.terminal_mode = terminal_mode
+        # Ftp username and password.
+        self.ftp_user = ftp_user
+        self.ftp_password = ftp_password
         # Initialization parameters.
         self.url = url
         self.filename = filename
-        self.log_filename = filename + '._temp_log'
+        self.work_path = work_path
+        self.log_filename = work_path + filename + '._temp_log'
         self.block_size = int(block_size)
         self.block_list = []
         self.headers = headers
@@ -40,7 +46,7 @@ class Downloader:
         self.file_type = 'Unknown'
         self.server_code = 0
         # Progress speed.
-        self._speed_time = 0
+        self._now_time = 0
         self._speed_pre_second = 0
         # Error detect
         self._error_timer = 0
@@ -66,17 +72,9 @@ class Downloader:
             # Save server code.
             self.server_code = f.getcode()
 
-    def support_resume(self):
-        if self.server_code == 0:
-            self.get_server_info()
-        if self.server_code == 206:
-            return True
-        else:
-            return False
-
-    def download(self):
+    def http_download(self):
         self.get_server_info()
-        if self.support_resume():
+        if self._support_resume() and self.info['Content-Length'] == 2 or '2':
             # Create block list.
             self._create_block_list()
             # Check log file.
@@ -87,7 +85,7 @@ class Downloader:
                 if start < self.content_length_now:
                     continue
                 # Try download.
-                self._speed_time = time.time()
+                self._now_time = time.time()
                 self._log_save(start)
                 self._download(start, end)
                 # Update progress.
@@ -102,7 +100,7 @@ class Downloader:
         else:
             self._download(self.content_length_now, self.content_length_size)
 
-    def re_download(self):
+    def http_re_download(self):
         try:
             self.get_server_info()
         except Exception as e:
@@ -178,6 +176,14 @@ class Downloader:
             sys.stdout.flush()
         print('%s completed' % self.filename)
 
+    def _support_resume(self):
+        if self.server_code == 0:
+            self.get_server_info()
+        if self.server_code == 206:
+            return True
+        else:
+            return False
+
     def _create_block_list(self):
         # Attention: request length is closed interval,but python array is different.
         block_number = int((self.content_length_size - self.content_length_now) / self.block_size) + 1
@@ -193,7 +199,7 @@ class Downloader:
     def _progress_bar(self):
         progress = (self.content_length_now / self.content_length_size)
         size_mb = self.content_length_size / (1024 * 1024)
-        _time = (time.time() - self._speed_time)
+        _time = (time.time() - self._now_time)
         self._speed_pre_second = int(self.block_size) / (_time * 1024 * 1024)
         # Print in terminal or python interpreter.
         if self.terminal_mode:
@@ -212,39 +218,67 @@ class Downloader:
         byte_range = 'bytes=' + str(begin) + '-' + str(end - 1)
         req.add_header('Range', byte_range)
         with urllib.request.urlopen(req, context=self.ssl_context, timeout=self.timeout) as r:
-            with open(self.filename, mode='ab+') as f:
+            with open(self.work_path + self.filename, mode='ab+') as f:
                 f.seek(self.content_length_now)
                 f.truncate()
                 f.write(r.read())
 
-    def _url_open(self):
-        req = urllib.request.Request(self.url)
-        if self.headers:
-            for k, v in self.headers.items():
-                req.add_header(k, v)
-        with urllib.request.urlopen(req, context=self.ssl_context, timeout=self.timeout, data={}) as r:
-            return r.read()
+    def ftp_download(self):
+        # Extract ip address and host.
+        host = self.url.split('://')[1].split('/')[0]
+        ip = socket.getaddrinfo(host=host, port='21')[0][4][0]
+        # Get filename and directory.
+        filename = self.url.split('://')[1].split('/')[-1]
+        if self.filename == '':
+            self.filename = filename
+        directory = self.url.split('://')[1].split('/', 1)[1].split(filename, 1)[0]
+        # Begin retrieve.
+        if self.ftp_user == '' and self.ftp_password == '':
+            # Simple way.
+            self._now_time = time.time()
+            ftp = urllib.request.URLopener()
+            ftp.retrieve(url, filename, reporthook=self._ftp_progress)
+        else:
+            # If ftp serve need user and password.
+            with ftplib.FTP(host=ip, user=self.ftp_user, passwd=self.ftp_password) as ftp:
+                with open(self.work_path + self.filename, mode='ab+') as f:
+                    ftp.login()
+                    ftp.cwd('%s' % directory)
+                    ftp.retrbinary('RETR %s' % filename, f.write, blocksize=self.block_size)
+            self._download_success()
 
-    def test(self):
-        # Only for test.
-        self.get_server_info()
-        for k, v in self.info.items():
-            print('+%s : %s' % (k, v))
-            if k == 'filename':
-                print('22222222222222')
+    def download(self):
+        if self.url.split('://')[0] == 'ftp':
+            self.ftp_download()
+        elif self.url.split('://')[0] == 'http' or 'https':
+            self.http_download()
+        else:
+            print('Unknown protocol!')
+
+    def _ftp_progress(self, _chunk_num, _chunk_size, _file_size):
+        self.block_size = _chunk_size
+        self.content_length_now = _chunk_num * _chunk_size
+        self.content_length_size = _file_size
+        if self.content_length_now > self.content_length_size:
+            self.content_length_now = self.content_length_size
+            self._download_success()
+        else:
+            self._progress_bar()
+        self._now_time = time.time()
 
 
 if __name__ == "__main__":
-    # url = 'http://www.dy2018.com/i/96869.html'
+    url = 'http://www.dy2018.com/i/96869.html'
+    url = 'http://www.cau.edu.cn/picture/0/1612102242504268107.jpg'
     # url = 'http://tug.org/cgi-bin/mactex-download/MacTeX.pkg'
     # url = 'ed2k://|file|[算法（第四版）.中文版.图灵程序设计丛书]Algorithms.-.Fourth.Edition.谢路云.影印版（高清）.pdf'
     # url = 'https://download.jetbrains.8686c.com/idea/ideaIU-2016.3.2.dmg'
+    url = 'ftp://ftp.cau.edu.cn/Upload/FOR_MAC/Programming/RStudio-0.98.1091.dmg'
+    # url = 'ftp://8:8@xia.dl1234.com:8807/[电影天堂www.dy2018.com]美国队长3DVD中英双字.rmvb'
     # print(url)
 
-    headers = {
-        'User-Agent=': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
-    }
-    a = Downloader('http://202.112.175.202:8080/poweb/downloadisofile?isoid=1956', block_size=1024 * 1024,
-                   terminal_mode=False, timeout=1)
+    a = Downloader(url,
+                   work_path='/Users/zyh/github/Mini-Spider/test/',
+                   block_size=1024 * 1,
+                   terminal_mode=True, timeout=10)
     a.download()
-    a.test()
