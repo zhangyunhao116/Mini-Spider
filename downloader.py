2bd79d747acb33fdc7aded3c824ff31ee4aaa3f4
ZYunH <workvl@163.com>
2017-03-13 15:07:23 +0800

    ver 0.0.1
    For downloader
    1. Add url check function.
    2. Add work path and check exist file function.
    3. Add a simple request header
    4. Fix some bugs.

--- downloader.py
+++ downloader.py
@@ -14,12 +14,20 @@ class Downloader:
     """This class is used to handle the Download requests that arrive at the server."""
 
     def __init__(self, url, filename='', work_path='', block_size=1024 * 1024, headers={}, timeout=2, ssl_context=None,
-                 terminal_mode=False, ftp_user='', ftp_password=''):
+                 terminal_mode=False, url_check=True, ftp_user='', ftp_password=''):
+        # Parse chinese to ascii and delete parameters.
+        self.url = url
+        self.url_check = url_check
+        self._check_url()
         # If filename is not specified,it is equal to the default value provided by url.
         if filename == '':
-            filename = url.split('/')[-1]
-        # Parse chinese to ascii.
-        url = urllib.parse.quote(url, safe='/:?=@&[]')
+            filename = self.url.split('/')[-1]
+        # Check work directory.
+        self.work_path = work_path
+        self._check_work_path()
+        # Check headers if not exist.
+        self.headers = headers
+        self._check_headers()
         # Create ssl context.
         if ssl_context:
             self.ssl_context = ssl_context
@@ -31,13 +39,10 @@ class Downloader:
         self.ftp_user = ftp_user
         self.ftp_password = ftp_password
         # Initialization parameters.
-        self.url = url
         self.filename = filename
-        self.work_path = work_path
-        self.log_filename = work_path + filename + '._temp_log'
+        self.log_filename = self.work_path + self.filename + '._temp_log'
         self.block_size = int(block_size)
         self.block_list = []
-        self.headers = headers
         self.timeout = timeout
         self.content_length_now = 0
         self.content_length_size = 0
@@ -50,6 +55,14 @@ class Downloader:
         self._speed_pre_second = 0
         # Error detect
         self._error_timer = 0
+
+    def download(self):
+        if self.url.split('://')[0] == 'ftp':
+            self.ftp_download()
+        elif self.url.split('://')[0] == 'http' or 'https':
+            self.http_download()
+        else:
+            print('Unknown protocol!')
 
     def get_server_info(self):
         req = urllib.request.Request(self.url)
@@ -74,6 +87,11 @@ class Downloader:
 
     def http_download(self):
         self.get_server_info()
+        # If file is completed, pass.
+        if self._check_file():
+            print('%s is already completed!' % self.filename)
+            return True
+        # If server support resume function.
         if self._support_resume() and self.info['Content-Length'] == 2 or '2':
             # Create block list.
             self._create_block_list()
@@ -100,41 +118,37 @@ class Downloader:
         else:
             self._download(self.content_length_now, self.content_length_size)
 
-    def http_re_download(self):
-        try:
-            self.get_server_info()
-        except Exception as e:
-            print(e)
-            self._error()
-            self.re_download()
-            return 0
-        try:
-            if self.content_length_size == os.path.getsize(self.filename):
-                return 0
-        except Exception as e:
-            print(e)
-            self._error()
-            pass
-        while self.content_length_now != self.content_length_size:
-            try:
-                self.download()
-            except Exception as e:
-                print(e)
-                self._error()
-                return 0
+    def ftp_download(self):
+        # Extract ip address and host.
+        host = self.url.split('://')[1].split('/')[0]
+        ip = socket.getaddrinfo(host=host, port='21')[0][4][0]
+        # Get filename and directory.
+        filename = self.url.split('://')[1].split('/')[-1]
+        if self.filename == '':
+            self.filename = filename
+        directory = self.url.split('://')[1].split('/', 1)[1].split(filename, 1)[0]
+        # Begin retrieve.
+        if self.ftp_user == '' and self.ftp_password == '':
+            # Simple way.
+            self._now_time = time.time()
+            ftp = urllib.request.URLopener()
+            ftp.retrieve(url, self.work_path + self.filename, reporthook=self._ftp_progress)
+        else:
+            # If ftp serve need user and password.
+            with ftplib.FTP(host=ip, user=self.ftp_user, passwd=self.ftp_password) as ftp:
+                with open(self.work_path + self.filename, mode='ab+', buffering=self.block_size) as f:
+                    ftp.login()
+                    ftp.cwd('%s' % directory)
+                    ftp.retrbinary('RETR %s' % filename, f.write, blocksize=self.block_size)
+            self._download_success()
 
-    def _headers_read(self, filename):
-        result = []
-
-        with open(filename, 'r') as content:
-            for line in content:
-                if line[-1] == '\n':
-                    line = line[0:-1]
-                result.append(line)
-
-        for i in result:
-            temp = i.split(':')
-            self.headers[temp[0]] = temp[1]
+    def _check_headers(self):
+        _headers_chrome = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        }
+        if self.headers == {}:
+            self.headers = _headers_chrome
 
     def _log_save(self, content_length):
         content = [content_length]
@@ -197,6 +211,8 @@ class Downloader:
             self.block_list.append(tuple(temp))
 
     def _progress_bar(self):
+        if self.content_length_size == 0:
+            return False
         progress = (self.content_length_now / self.content_length_size)
         size_mb = self.content_length_size / (1024 * 1024)
         _time = (time.time() - self._now_time)
@@ -218,42 +234,10 @@ class Downloader:
         byte_range = 'bytes=' + str(begin) + '-' + str(end - 1)
         req.add_header('Range', byte_range)
         with urllib.request.urlopen(req, context=self.ssl_context, timeout=self.timeout) as r:
-            with open(self.work_path + self.filename, mode='ab+') as f:
+            with open(self.work_path + self.filename, mode='ab+', buffering=self.block_size) as f:
                 f.seek(self.content_length_now)
                 f.truncate()
                 f.write(r.read())
-
-    def ftp_download(self):
-        # Extract ip address and host.
-        host = self.url.split('://')[1].split('/')[0]
-        ip = socket.getaddrinfo(host=host, port='21')[0][4][0]
-        # Get filename and directory.
-        filename = self.url.split('://')[1].split('/')[-1]
-        if self.filename == '':
-            self.filename = filename
-        directory = self.url.split('://')[1].split('/', 1)[1].split(filename, 1)[0]
-        # Begin retrieve.
-        if self.ftp_user == '' and self.ftp_password == '':
-            # Simple way.
-            self._now_time = time.time()
-            ftp = urllib.request.URLopener()
-            ftp.retrieve(url, filename, reporthook=self._ftp_progress)
-        else:
-            # If ftp serve need user and password.
-            with ftplib.FTP(host=ip, user=self.ftp_user, passwd=self.ftp_password) as ftp:
-                with open(self.work_path + self.filename, mode='ab+') as f:
-                    ftp.login()
-                    ftp.cwd('%s' % directory)
-                    ftp.retrbinary('RETR %s' % filename, f.write, blocksize=self.block_size)
-            self._download_success()
-
-    def download(self):
-        if self.url.split('://')[0] == 'ftp':
-            self.ftp_download()
-        elif self.url.split('://')[0] == 'http' or 'https':
-            self.http_download()
-        else:
-            print('Unknown protocol!')
 
     def _ftp_progress(self, _chunk_num, _chunk_size, _file_size):
         self.block_size = _chunk_size
@@ -266,19 +250,45 @@ class Downloader:
             self._progress_bar()
         self._now_time = time.time()
 
+    def _check_file(self):
+        try:
+            file_size = os.path.getsize(self.work_path + self.filename)
+            if file_size == self.content_length_size:
+                return True
+            else:
+                return False
+        except FileNotFoundError:
+            return False
+
+    def _check_work_path(self):
+        try:
+            os.chdir(self.work_path)
+        except FileNotFoundError:
+            os.mkdir(self.work_path)
+        if self.work_path[-1] != '/':
+            self.work_path = self.work_path + '/'
+
+    def _check_url(self):
+        # Delete get parameters.
+        if self.url_check:
+            self.url = self.url.split('?', 1)[0]
+        # Parse chinese to ascii.
+        self.url = urllib.parse.quote(self.url, safe='/:?=@&[]')
+
 
 if __name__ == "__main__":
     url = 'http://www.dy2018.com/i/96869.html'
     url = 'http://www.cau.edu.cn/picture/0/1612102242504268107.jpg'
     # url = 'http://tug.org/cgi-bin/mactex-download/MacTeX.pkg'
-    # url = 'ed2k://|file|[算法（第四版）.中文版.图灵程序设计丛书]Algorithms.-.Fourth.Edition.谢路云.影印版（高清）.pdf'
     # url = 'https://download.jetbrains.8686c.com/idea/ideaIU-2016.3.2.dmg'
-    url = 'ftp://ftp.cau.edu.cn/Upload/FOR_MAC/Programming/RStudio-0.98.1091.dmg'
+    # url = 'ftp://ftp.cau.edu.cn/Upload/FOR_MAC/Programming/RStudio-0.98.1091.dmg'
     # url = 'ftp://8:8@xia.dl1234.com:8807/[电影天堂www.dy2018.com]美国队长3DVD中英双字.rmvb'
-    # print(url)
+    url = 'http://pt.cau.edu.cn/download.php?tid=155042'
 
     a = Downloader(url,
-                   work_path='/Users/zyh/github/Mini-Spider/test/',
-                   block_size=1024 * 1,
-                   terminal_mode=True, timeout=10)
+                   work_path='/Users/zyh/github/Mini-Spider/test',
+                   block_size=1024 * 1024 * 10,
+                   terminal_mode=False,
+                   url_check=False
+                   )
     a.download()
